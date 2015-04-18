0a0470c4012e3c639c4d2331a044eab0dae1194e
Robson Gian Perassoli <robsonperassoli@gmail.com>
2015-04-18 13:14:32 -0300

    Coolest unit tests evergit statusgit status

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+	
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'precisa conter a url no formato GIF, PNG ou JPG'	
+	}
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_product(image_url)
+  	Product.new(title: 'Dell inspiron 1545', 
+  		description: 'Too slow', 
+  		price: 1, 
+  		image_url: image_url)
+  end
+
+  test "product attributes must be not empty" do
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:price].any?
+  	assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+  	product = Product.new(title: 'Nootebook', 
+				  		  description: "Just another notebook...", 
+				  		  image_url: "some_image.jpg")
+  	price_validation_message = ["must be greater than or equal to 0.01"]
+
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal price_validation_message, product.errors[:price]
+
+  	product.price = 0
+  	assert product.invalid?
+  	assert_equal price_validation_message, product.errors[:price]
+
+  	product.price = 1
+  	assert product.valid?
+  end
+
+  test "image url" do
+  	ok = %w{insp1545.jpg insp1545.gif insp1545.png INSP1545.jpg INSP1545.gif INSP1545.png 
+  		http://somedomain.com/image.gif}
+  	bad = %w{insp.doc insp.gif/more insp.gif.more}
+  	ok.each do |image_name|
+  		assert new_product(image_name).valid?, "#{image_name} should be valid"
+  	end
+
+  	bad.each do |image_name|
+  		assert new_product(image_name).invalid?, "#{image_name} should not be vaalid"
+  	end
+  end
+
+  test "product is not valid without a unique name" do
+  	product = Product.new(title: products(:insp1545).title,
+  		description: 'this is just another description',
+  		price: 2500.00,
+  		image_url: 'someimage.jpg')
+  	assert product.invalid?
+  	assert_equal ["has already been taken"], product.errors[:title]
+  end
 end
